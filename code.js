9f857790e4d1828fc55378ef7b33bc42c6b31e1a
Indigo-bit <128424965+Indigo-bit@users.noreply.github.com>
2023-05-26 20:26:08 -0600

    agregue mis archivos

---
+++ code.js
@@ -0,0 +1,106 @@
+ 
+function shownav(){
+    document.getElementById('navshow').style.display='block'
+
+
+}
+
+function hidenav(){
+    document.getElementById('navshow').style.display='none'
+}
+
+// const navblock = document.getElementById('official-sites-block')
+
+
+function showblock(){
+    const navblock = document.getElementById('official-sites-block')
+
+    navblock.classList.toggle('show-sitesblock')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// window.onclick = (event)=> {
+
+//     if(!event.target.matches('.nav-button')){
+//         if(navblock.classList.contains('show-sitesblock')){
+//             navblock.classList.remove('show-sitesblock')
+//         }
+//     }
+// }
+
+
+
+// window.onclick=function(){
+//     var navblock= document.getElementById('official-sites-block');
+//     document.onclick=function(div){
+//         if(div.target.id ==='official-sites-btn'){
+//             navblock.style.display='block'
+//         }else{
+//             navblock.style.display='none'
+//         }
+
+
+//     }
+// }
+
+ 
+
+
+
+
+// var sitesbtn= document.getElementsByClassName('.nav-button')[0];
+// var sitesblock= document.getElementsByClassName('official-sites-select')[0];
+
+
+
+ 
+// function showblock(){
+//      document.getElementById('official-sites-block').style.display = 'block';
+//      if(document.getElementById('official-sites-block').style.display='none'){
+//         document.getElementById('official-sites-block').style.display='block';
+//      }
+
+// }
+
+// // function hideblock(){
+// //     document.getElementById('official-sites-block').style.display='none'
+// //     document.getElementById('button-close').style.display='none'
+// // }
+
+
